ea6b69d129badc76b0681f30a17e75e191f603e7
Alistair A. Israel <aisrael@gmail.com>
2018-02-19 18:00:13 +0800

    Added a simple, hierarchical, global logging system

--- spec/logging_spec.cr
+++ spec/logging_spec.cr
@@ -1,9 +1,22 @@
 require "./spec_helper"
 
-describe Logging do
-  # TODO: Write tests
+class Foo
+  include Logging
 
+  def foo
+    debug "Debug message"
+  end
+end
+
+describe Logging do
   it "works" do
-    false.should eq(true)
+    Logging::Config.root_level = Logger::DEBUG
+    s = String.build(1024) do |sb|
+      Logging::Config.root_log_to = sb
+      f = Foo.new
+      f.foo
+    end.chomp
+    expected_pattern = %r(^D, \[\d{4,4}-\d{2,2}-\d{2,2} \d{2,2}:\d{2,2}:\d{2,2} \+\d{2,2}:\d{2,2} #\d+\] DEBUG -- : /spec/logging_spec.cr:7 Debug message)
+    s.should match(expected_pattern)
   end
 end
--- src/logging.cr
+++ src/logging.cr
@@ -1,6 +1,45 @@
+require "logger"
 require "./logging/*"
 
-# TODO: Write documentation for `Logging`
 module Logging
-  # TODO: Put your code here
+  {% for name in Logger::Severity.constants %}
+    macro self.{{name.id.downcase}}(message)
+      Logging::Config.get_logger(self.to_s).{{name.id.downcase}}(%(#{__FILE__[Dir.current.size..-1]}:#{__LINE__} ) + \{{message}})
+    end
+    macro {{name.id.downcase}}(message)
+      Logging::Config.get_logger(self.class.to_s).{{name.id.downcase}}(%(#{__FILE__[Dir.current.size..-1]}:#{__LINE__} ) + \{{message}})
+    end
+  {% end %}
+  class Config
+    def self.root_level=(level : Logger::Severity)
+      @@root_logger.level = level
+    end
+    def self.root_level : Logger::Severity
+      @@root_logger.level
+    end
+    def self.root_log_to=(io : IO)
+      old_level = @@root_logger.level
+      @@root_logger = Logger.new(io)
+      @@root_logger.level = old_level
+    end
+    def self.get_logger(name)
+      parts = name.split("::")
+      found = parts.size.downto(0).find do |i|
+        key = parts[0..i].join("/")
+        @@loggers.has_key?(key)
+      end
+      if found
+        key = parts[0..found].join("/")
+        @@loggers[key].not_nil!
+      else
+        @@root_logger
+      end
+    end
+    @@root_logger : Logger = Logger.new(STDOUT).tap do |logger|
+      logger.level = Logger::WARN
+    end
+    @@loggers : Hash(String, Logger) = Hash(String, Logger).new.tap do |h|
+      h[""] = @@root_logger
+    end
+  end
 end
